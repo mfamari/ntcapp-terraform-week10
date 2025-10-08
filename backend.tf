35ed6f9cf59d0fb0be024b076b4e5c9d1801e8a5
babually9 <babually9@gmail.com>
2025-10-07 21:47:38 -0400

    test pipeline

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "w7-ma-terraform-bucket"
-    key          = "week10/terraform.tfstate"
-    region       = "us-east-1"
-    use_lockfile = false
-  }
-}
